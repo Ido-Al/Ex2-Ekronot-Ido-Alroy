a06b97e0e0e9a94ec8e5a1da3968850991c088f5
ido Alroy <ido.alroy@gmail.com>
2024-11-16 13:57:58 +0200

    added documentation

--- Ex2-Ekronot-IdoAlroy/User.h
+++ Ex2-Ekronot-IdoAlroy/User.h
@@ -6,16 +6,16 @@ class User
 {
 public:
 	// constractor and clear
-	void init(unsigned int id, std::string username, unsigned int age);
-	void clear();
+	void init(unsigned int id, std::string username, unsigned int age); // function to initate a user
+	void clear(); // function to clear the devices of a user
 	// getters
-	unsigned int getID() const;
-	std::string getUserName() const;
-	unsigned int getAge() const;
-	DevicesList& getDevices();
+	unsigned int getID() const; // function to  get the id of the user
+	std::string getUserName() const; // function to get the username
+	unsigned int getAge() const; // function to get the age
+	DevicesList& getDevices(); // function to get the devices 
 	// device actions
-	void addDevice(Device newDevice);
-	bool checkIfDevicesAreOn() const;
+	void addDevice(Device newDevice); // function to add a device
+	bool checkIfDevicesAreOn() const; // function to check if all the devices of the user are activated
 private:
 	unsigned int _id;
 	std::string _username;
